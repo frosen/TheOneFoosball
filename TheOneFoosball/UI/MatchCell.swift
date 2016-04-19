4cce68d9aaa14adedc1802d669fa7d7a0cdfc950
卢乐颜 <frosen_lu@126.com>
2016-04-20 07:59:57 +0800

    add

---
+++ TheOneFoosball/UI/MatchCell.swift
@@ -0,0 +1,27 @@
+//
+//  MatchCell.swift
+//  TheOneFoosball
+//
+//  Created by 卢乐颜 on 16/4/19.
+//  Copyright © 2016年 luleyan. All rights reserved.
+//
+
+import UIKit
+
+class MatchCell: UITableViewCell {
+
+    @IBOutlet weak var matchName: UILabel!
+
+    @IBOutlet weak var leftTeamName: UILabel!
+    @IBOutlet weak var rightTeamName: UILabel!
+
+    @IBOutlet weak var leftTeamScore: UILabel!
+    @IBOutlet weak var rightTeamScore: UILabel!
+
+    @IBOutlet weak var scoreDetail: UILabel!
+
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var finishShow: UILabel!
+    
+
+}
